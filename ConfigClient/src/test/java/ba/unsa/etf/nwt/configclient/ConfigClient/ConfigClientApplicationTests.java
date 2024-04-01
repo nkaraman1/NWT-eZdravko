2dc31f76d7ece7dbc87c66feb2b96296403a3b49
Elvedin Smajić <esmajic2@etf.unsa.ba>
2024-04-01 23:09:52 +0200

    Odvojene test i dev konfiguracije za UserMS

---
+++ ConfigClient/src/test/java/ba/unsa/etf/nwt/configclient/ConfigClient/ConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.nwt.configclient.ConfigClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
