702e9eec6a4e5567f3c22dc886abcf3a4644edee
Elvedin Smajić <esmajic2@etf.unsa.ba>
2024-05-07 00:18:10 +0200

    Podesen API gateway za mikroservise

---
+++ APIGateway/src/test/java/ba/unsa/etf/nwt/APIGateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.nwt.APIGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- README.md
+++ README.md
@@ -8,6 +8,7 @@ Config repozitorij:
 <br/><br/>
 Portovi koji se koriste za servise:
 <list>
+  <li>API Gateway: 8080</li>
   <li>Config Server: 8888</li>
   <li>Eureka Server: 8761</li>
   <li>Ostali servisi dobijaju random port od Eureke</li>
