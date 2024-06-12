2b3de9bda432bd510b70905c35c5f83bdc53a071
nkaraman1 <nkaraman1@etf.unsa.ba>
2024-06-12 09:59:21 +0200

    Promjena u sql fajlu za patient service

--- PatientService/src/main/resources/default.sql
+++ PatientService/src/main/resources/default.sql
@@ -10,17 +10,17 @@ VALUES (2, 1, '2024-03-19', 165.5, 58, 23, 70, 1.5, 6500);
 
 --pregledi
 INSERT INTO Pregledi(id, pacijent_uid, doktor_uid, dijagnoza, termin_pregleda)
-VALUES (1, 2, 1, "promaha", "2024-03-15, 17:58");
+VALUES (1, 2, 1, "promaha", "2024-03-15 17:58");
 
 INSERT INTO Pregledi(id, pacijent_uid, doktor_uid, dijagnoza, termin_pregleda)
-VALUES (2, 2, 1, "ograisanje", "2024-03-19, 10:03");
+VALUES (2, 2, 1, "ograisanje", "2024-03-19 10:03");
 
 --uputnice
-INSERT INTO Uputnice(id, pregled_id, specijalista_uid, komentar, trajanje)
-VALUES (1, 1, 2, 'molim savjet strucnjaka', "14?");
+INSERT INTO Uputnice(id, pregled_id, specijalista_uid, komentar, datum_isteka)
+VALUES (1, 1, 2, 'molim savjet strucnjaka', "2024-04-04");
 
-INSERT INTO Uputnice(id, pregled_id, specijalista_uid, komentar, trajanje)
-VALUES (2, 1, 2, 'molim savjet strucnjaka', "14?");
+INSERT INTO Uputnice(id, pregled_id, specijalista_uid, komentar, datum_isteka)
+VALUES (2, 1, 2, 'molim savjet strucnjaka', "2024-04-02");
 
 --tip nalaza
 INSERT INTO tip_nalaza(id, naziv)
@@ -37,10 +37,10 @@ INSERT INTO nalazi(id, pacijent_uid, laborant_uid, doktor_uid, tip_nalaza_id, di
 VALUES (1, 2, 2, 1, 1, null, null);
 
 INSERT INTO nalazi(id, pacijent_uid, laborant_uid, doktor_uid, tip_nalaza_id, dijagnoza, vrijeme_pregleda)
-VALUES (2, 2, 2, 1, 2, "nije dobro", "2024-03-19, 10:03");
+VALUES (2, 2, 2, 1, 2, "nije dobro", "2024-03-19 10:03");
 
 INSERT INTO nalazi(id, pacijent_uid, laborant_uid, doktor_uid, tip_nalaza_id, dijagnoza, vrijeme_pregleda)
-VALUES (3, 2, 2, 1, 3, "nije lose", "2024-03-19, 12:45");
+VALUES (3, 2, 2, 1, 3, "nije lose", "2024-03-19 12:45");
 
 --stavke
 INSERT INTO stavke(id, naziv, ref_min, ref_max, ref, mjerna_jedinica, tip_nalaza_id)
